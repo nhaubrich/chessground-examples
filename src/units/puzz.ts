962b19c26a219be5b328c7a1d50c1f0f14d26e36
Nick Haubrich <nicholasjhaubrich@gmail.com>
2025-02-04 15:25:04 -0500

    Add looping puzzles for given rating

--- src/units/puzz.ts
+++ src/units/puzz.ts
@@ -6,27 +6,45 @@ import {  toColor, toOppositeColor, toDests, san_to_uci} from '../util'
 //import { toColor, toDests,  playOtherSide, san_to_uci} from '../util'
 import { uciToMove } from 'chessground/util';
 
-/* Host puzzles gotten from lichess, ideally with given puzzleElo.
- * Lichess gives move list. Need to iterate through (no animation, immovable)
+/* Host puzzles gotten from lichess. increment elo on correct solution
+ * Can't fetch puzzle by rating directly. Make database of puzzle IDs and ratings, categorize into bins, and select relevant IDs
  * */
 export const puzzle: Unit = {
   name: 'Puzzle',
   run(el) {
-    //const fen = '1nbqk2r/pppp1ppp/5n2/2b1p3/2B1P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 4 4';
-    //const chess = new Chess(fen);
-    const chess = new Chess();
+    var chess = new Chess();
     //first fetch puzzle and play moves
     
+    async function getPuzzleIdByRating(rating) {
+      const tableResponse = await fetch("../../assets/puzzleIds.json");
+      console.log(tableResponse); 
+      const puzzleIdTable = await tableResponse.json();
+      console.log(puzzleIdTable);
+
+      const ratingBin = Math.max( Math.min( Math.round(rating/50)*50, 3000),400);
+      const puzzleIds = puzzleIdTable["PuzzleId"][ratingBin];
+      //const puzzleIds = puzzleIdTable[String(ratingBin)];
+      
+      const randomIdx = Math.floor(Math.random() * puzzleIds.length);
+      return puzzleIds[randomIdx]
+    }
+
     async function fetchData() {
-      const response = await fetch('https://lichess.org/api/puzzle/next');
+      chess = new Chess();
+      console.log(chess.fen())
+
+      var response = await fetch('https://lichess.org/api/puzzle/next');
+      if(true){ 
+          const puzzleId = await getPuzzleIdByRating(600);
+          console.log(puzzleId)
+          response = await fetch('https://lichess.org/api/puzzle/'+puzzleId);
+          console.log(response);
+      }
       const data = await response.json();
-      /*console.log(data);*/
       console.log(data.puzzle);
-      //console.log(data.puzzle.initialPly);
       var moveList = data.game.pgn.split(" ")
       
       for (var i=0; i < data.puzzle.initialPly; i++) {
-        //console.log(moveList[i]);
         chess.move(moveList[i]);
       };
       return [chess,data];
@@ -35,8 +53,6 @@ export const puzzle: Unit = {
         const [chess,data] = await fetchData();
         const fen = chess.fen()
         console.log(chess.fen());
-        //const startingMoves = chess.history().length+1; //increment for initial played move
-        //console.log(startingMoves);
         var moveIdx = 0;
         const cg = Chessground(el, {
           fen: fen,
@@ -49,8 +65,6 @@ export const puzzle: Unit = {
           }
         });
         console.log(data);
-        //console.log(chess.moves());//i.e. legal moves
-        //console.log(chess.history());//all the ply
         
         //make initial move
         var moveList = data.game.pgn.split(" ");
@@ -69,20 +83,9 @@ export const puzzle: Unit = {
           }
         });
         
-
-        //checkPuzzle(cg, chess,data, startingMoves);//idk
         cg.set({
           movable: { events: { after: checkPuzzle(cg, chess,data, moveIdx) } }
         });
-        //cg.set({
-        //  movable: { events: { after: checkPuzzle(cg, chess,data, startingMoves) } }
-        //});
-        //cg.set({
-        //  movable: { events: { after: checkPuzzle(cg, chess,data, startingMoves) } }
-        //});
-        //if (false){
-        //    checkPuzzle(cg,chess,data,startingMoves);
-        //}
         return cg;
     }
     function checkPuzzle(cg,chess,data,moveIdx){
@@ -91,7 +94,7 @@ export const puzzle: Unit = {
         console.log(orig,dest);
         
         console.log(chess.fen())
-        let output = chess.move({from: orig, to: dest, promotion: 'q'});//unable to select promotion ???
+        let output = chess.move({from: orig, to: dest, promotion: 'q'});//unable to select promotion ??? consider https://github.com/hi-ogawa/chessground-promotion
         console.log(output);
         console.log(chess.fen())
         //const plyCount = chess.history().length;
@@ -132,27 +135,13 @@ export const puzzle: Unit = {
                         enabled: false,
                       },
                     });
-
+                    displayPuzzle();
                 }
                 moveIdx=moveIdx+2;
-
-                
             }else {
                 console.log("wrong move")
                 //undo move on chess and cg?
                 chess.undo();
-                //cg.move(dest,orig);//
-                
-                //cg = Chessground(el, {
-                //  fen: chess.fen(),
-                //  turnColor: toColor(chess),
-                //  orientation: toColor(chess),
-                //  movable: {
-                //    color: toColor(chess),
-                //    free: false,
-                //    dests: toDests(chess)
-                //  }
-                //});
                 
                 cg.set({
                   fen: chess.fen(),
@@ -164,10 +153,8 @@ export const puzzle: Unit = {
                 });
             }
         }
-        //check if last move is correct. first move after setup should be first soln move. compare last move to soln[move number - init move count]
         };
     }
-
     displayPuzzle();
 
     //dummy return. Can I remove this?
