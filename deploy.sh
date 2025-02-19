3aa8ab511ce76135df3632ccc9ed0253e128ad48
Nick Haubrich <nicholasjhaubrich@gmail.com>
2025-02-19 11:10:47 -0500

    Remove unnecessary files from deployment

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 npm run build
-VERSION=1.2
+VERSION=1.3
 THEDIR="puzzles"
 sed -i -e "s/..\/..\/assets/\/$THEDIR\/assets/" dist/chessground-examples.js
 mv dist/chessground-examples.js dist/chessground-examples.$VERSION.js
 sed -i -e "s/chessground-examples.js/chessground-examples.$VERSION.js/" index.html
-rsync -r * website:~/public_html/$THEDIR
+rsync -r assets dist index.html node_modules website:~/public_html/$THEDIR
